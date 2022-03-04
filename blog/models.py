fa6d6d8192f60e7a898896d92bd2091838152adc
Bryson Steck <steck.bryson@gmail.com>
2022-03-04 16:58:10 -0700

    fixed migration and implemented markdownfield

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from markdownfield.models import MarkdownField, RenderedMarkdownField
+from markdownfield.validators import VALIDATOR_STANDARD
 
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    content = models.TextField()
+    text = MarkdownField(rendered_field='text_rendered',use_editor=False, use_admin_editor=True, validator=VALIDATOR_STANDARD)
+    text_rendered = RenderedMarkdownField()
     posted = models.DateTimeField('date posted')
