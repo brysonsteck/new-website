58b419d90d6dd0855985ccb3ad67ca25a0215d7f
Bryson Steck <steck.bryson@gmail.com>
2022-04-07 13:34:53 -0600

    going to fix layout more

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('archive/', views.archive, name='archive'),
     path('post/<int:blog_id>/', views.post, name='post'),
-    path('about/', views.about, name='about'),
 ]
