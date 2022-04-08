80818ac58dac053b04831d84aab6fd28370babe3
Bryson Steck <steck.bryson@gmail.com>
2022-04-08 00:38:34 -0600

    fixed time script

--- blog/static/blog/time.js
+++ blog/static/blog/time.js
@@ -5,24 +5,32 @@ function updateTime(){
   var date = new Date()
   var hours = date.getHours()
   var minutes = date.getMinutes()
+  var seconds = date.getSeconds()
+  var d_str = weekdayNames[date.getDay()] + ", " + monthNames[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear() + ", MST.";
 
   var old_hours = hours;
   if (hours > 12) {
     hours = hours - 12
+  } else if (hours == 0) {
+    hours = 12
   }
 
-  if (minutes < 10){
+  if (minutes < 10) {
     minutes = "0" + minutes
   }
 
-  var t_str = hours + ":" + minutes + " ";
-  if (old_hours > 11){
+  if (seconds < 10) {
+    seconds = "0" + seconds
+  }
+
+  var t_str = hours + ":" + minutes + ":" + seconds + " ";
+  if (old_hours > 11) {
     t_str += "p.m.";
   } else {
     t_str += "a.m.";
   }
 
-  document.getElementById('date').innerHTML = d_str + ".";
+  document.getElementById('date').innerHTML = d_str;
   document.getElementById('time').innerHTML = t_str;
 }
 setInterval(updateTime, 1);
